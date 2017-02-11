098b1c18c848ae3cc77932c279c8d87d58f095d4
manster829 <bghughes17@gmail.com>
2017-02-10 22:43:01 -0600

    idk

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-
-function clicked(){
-  console.log("it's been clicked");
-  document.getElementById("changetext").innerHTML = selectIt();
+function clicked() {
+    console.log("it's been clicked");
+    document.getElementById("changetext").innerHTML = selectIt();
 
 }
 
 var TextOptions = ["Bad Call", "That Was Silly", "I Can't Believe You've Done This", "Wow", "That Was Terrible", "Please Don't Do That Again", "You Have Goofed", "Uh Oh", "Can You Not?", "That Was A Mistake"];
 
-
-
-
-function selectIt(){
+function selectIt() {
     var rand = Math.round(Math.random() * 9);
-console.log(rand);
-        var elem = TextOptions[rand];
-
+    console.log(rand);
+    var elem = TextOptions[rand];
     return elem;
 }
 
